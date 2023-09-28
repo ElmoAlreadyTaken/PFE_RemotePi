29d735bcce3bb5d538a3a5b20b882e58ba616111
ElmoAlreadyTaken <78965119+ElmoAlreadyTaken@users.noreply.github.com>
2023-09-28 10:16:32 +0200

    feat : add interface in next.js and linked to a supabase server

---
+++ interface/pages/index.js
@@ -0,0 +1,18 @@
+import { supabase } from '../lib/supabase'
+
+export default function HomePage() {
+  async function fetchData() {
+    let { data, error } = await supabase.from('test_table').select('*')
+    if (data) {
+      console.log(data)
+    } else {
+      console.error(error)
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={fetchData}>Fetch Data</button>
+    </div>
+  )
+}
