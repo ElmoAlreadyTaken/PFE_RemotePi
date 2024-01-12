82c453b5c06c47303d5cf555356b0435769d5414
misha7a <misha123setta@gmail.com>
2024-01-12 13:06:06 +0100

    feat: add IDE home page

--- interface/pages/index.js
+++ interface/pages/index.js
@@ -1,20 +1,26 @@
 import { supabase } from '../lib/supabase'
 import FileUpload from '../components/FileUpload';
+import React from 'react';
+import AceEditor from 'react-ace';
+
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/theme-monokai';
 
 export default function HomePage() {
-  async function fetchData() {
-    let { data, error } = await supabase.from('test_table').select('*')
-    if (data) {
-      console.log(data)
-    } else {
-      console.error(error)
-    }
+  function onChange(newValue) {
+    console.log('change', newValue);
   }
 
   return (
-    <div>
-      <button onClick={fetchData}>Fetch Data</button>
-      <FileUpload />
+    <div className="aceEditorContainer">
+      <AceEditor
+        mode="javascript"
+        theme="monokai"
+        onChange={onChange}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+        className="aceEditor"
+      />
     </div>
   );
 }
