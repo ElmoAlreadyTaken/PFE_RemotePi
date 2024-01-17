1fba2976f4350db7cbb3e518f651fe3c5f53104b
ElmoAlreadyTaken <71309060+misha7a@users.noreply.github.com>
2024-01-17 14:31:38 +0100

    feat: add dynamic page if we are logged in or not

---
+++ interface/components/HomePage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import AceEditor from 'react-ace';
+import { supabase } from '../lib/supabase';
+import FileUpload from './FileUpload';
+
+import 'ace-builds/src-noconflict/mode-c_cpp';
+import 'ace-builds/src-noconflict/theme-monokai';
+
+export default function HomePage() {
+  function onChange(newValue) {
+    console.log('change', newValue);
+  }
+
+  return (
+    <div className="container">
+      <div className="streamContainer">
+        <iframe
+          src="https://player.twitch.tv/?channel=otplol_&parent=localhost"
+          height="720"
+          width="1280"
+          frameBorder="0"
+          scrolling="no"
+          allowFullScreen={true}>
+        </iframe>
+      </div>
+      <div className="fileUploadContainer">
+        <FileUpload />
+      </div>
+      <div className="aceEditorContainer">
+        <AceEditor
+          mode="c_cpp"
+          theme="monokai"
+          onChange={onChange}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{ $blockScrolling: true }}
+          className="aceEditor"
+        />
+      </div>
+    </div>
+  );
+}
--- interface/pages/index.js
+++ interface/pages/index.js
@@ -1,41 +1,46 @@
-import React from 'react';
-import AceEditor from 'react-ace';
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import HomePage from '../components/HomePage'; // Check this import
 import { supabase } from '../lib/supabase';
-import FileUpload from '../components/FileUpload';
 
-import 'ace-builds/src-noconflict/mode-c_cpp';
-import 'ace-builds/src-noconflict/theme-monokai';
+export default function MainComponent() {
+  const [loggedIn, setLoggedIn] = useState(false);
 
-export default function HomePage() {
-  function onChange(newValue) {
-    console.log('change', newValue);
-  }
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data: sessionData, error: sessionError } = await supabase.auth.getSession();
+      
+      if (sessionError) {
+        console.error('Error getting session:', sessionError);
+        return;
+      }
+
+      setLoggedIn(!!sessionData);
+    };
+
+    checkSession();
+
+    const authListener = supabase.auth.onAuthStateChange(async () => {
+      const { data: updatedSessionData } = await supabase.auth.getSession();
+      setLoggedIn(!!updatedSessionData);
+    });
+
+    // Check if unsubscribe method exists
+    return () => {
+      if (authListener && typeof authListener.unsubscribe === 'function') {
+        authListener.unsubscribe();
+      }
+    };
+  }, []);
 
   return (
-    <div className="container">
-      <div className="streamContainer">
-      <iframe
-        src="https://player.twitch.tv/?channel=domingo&parent=localhost"
-        height="720"
-        width="1280"
-        frameBorder="0"
-        scrolling="no"
-        allowFullScreen={true}>
-      </iframe>
+    <>
+      <Head>
+        <title>Register - Remote-PI</title>
+      </Head>
+      <div>
+        {loggedIn ? <HomePage /> : <div>Hello, CONNECTE TOI ENFLURE</div>}
       </div>
-      <div className="fileUploadContainer">
-        <FileUpload />
-      </div>
-      <div className="aceEditorContainer">
-        <AceEditor
-          mode="c_cpp"
-          theme="monokai"
-          onChange={onChange}
-          name="UNIQUE_ID_OF_DIV"
-          editorProps={{ $blockScrolling: true }}
-          className="aceEditor"
-        />
-      </div>
-    </div>
+    </>
   );
 }
