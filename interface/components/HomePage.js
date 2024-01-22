448e2e9e8129df141c9c276819a2efd430a92227
misha7a <misha123setta@gmail.com>
2024-01-22 12:44:32 +0100

    feat; add websockets (need to change IP adress when done)

--- interface/components/HomePage.js
+++ interface/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AceEditor from 'react-ace';
 import { useRouter } from 'next/router';
 import { supabase } from '../lib/supabase';
@@ -9,17 +9,46 @@ import 'ace-builds/src-noconflict/mode-c_cpp';
 import 'ace-builds/src-noconflict/theme-monokai';
 
 export default function HomePage(props) {
+  const [websocketData, setWebsocketData] = useState(null);
   const router = useRouter();
-  function onChange(newValue) {
-    console.log('change', newValue);
-  }
+
+  useEffect(() => {
+    // Remplacez 'wss://votre_adresse_ip_publique' par l'adresse de votre WebSocket
+    const ws = new WebSocket('wss://4.tcp.eu.ngrok.io:17707');
+
+    ws.onopen = () => {
+      console.log('WebSocket Connected');
+    };
+
+    ws.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      setWebsocketData(message);
+    };
+
+    ws.onerror = (error) => {
+      console.error('WebSocket Error: ', error);
+    };
+
+    ws.onclose = () => {
+      console.log('WebSocket Disconnected');
+    };
+
+    // Nettoyer la connexion WebSocket lorsque le composant est démonté
+    return () => {
+      ws.close();
+    };
+  }, []);
 
   const handleLogout = async () => {
     await supabase.auth.signOut();
     console.log("Disconnected");
     router.push('/');
-    
   };
+
+  function onChange(newValue) {
+    console.log('change', newValue);
+  }
+
 
   return (
     <div className="container">
@@ -36,6 +65,12 @@ export default function HomePage(props) {
       <div className="fileUploadContainer">
         <FileUpload />
       </div>
+      {websocketData && (
+        <div>
+          {/* Afficher les données reçues via WebSocket */}
+          <label>Données WebSocket: {JSON.stringify(websocketData)}</label>
+        </div>
+      )}
       <div className="aceEditorContainer">
         <AceEditor
           mode="c_cpp"
--- interface/pages/profile.js
+++ interface/pages/profile.js
@@ -1,36 +1,55 @@
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
+import { useRouter } from 'next/router';
 import { supabase } from "../lib/supabase";
 import styles from "../app/globals.css";
+import { data } from "autoprefixer";
 
 export default function Profile() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userProfile, setUserProfile] = useState(null);
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data: session } = await supabase.auth.getSession();
+      setIsLoggedIn(!!session);
+    };
+
+    checkSession();
+
+    const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setIsLoggedIn(!!session);
+    });
+
+    
+  }, []);
 
   useEffect(() => {
     const fetchProfile = async () => {
       try {
         setLoading(true);
-
-        if (supabase.auth) {
-          const user = supabase.auth.user;
-          console.log("la :", user);
-          if (user) {
-            const { data, error } = await supabase
-              .from("user_profiles")
-              .select()
-              .eq("user_id", user.id)
-              .single();
-
-            if (error) {
-              throw error;
-            }
-
-            setUserProfile(data);
-          } else {
-            // L'utilisateur n'est pas connecté, peut-être rediriger vers la page de connexion
-            // ou afficher un message invitant l'utilisateur à se connecter.
-          }
+  
+        // Fetch the current user
+        const { data: user, error: userError } = await supabase.auth.getUser();
+        if (userError) throw userError;
+  
+        if (user) {
+          // Fetch user profile data
+          const { data: profileData, error: profileError } = await supabase
+            .from("user_profiles")
+            .select()
+            .eq("user_id", user.user.id)
+            .single();
+          console.log("request : ",profileData);
+          console.log("user : ",user);
+          if (profileError) throw profileError;
+          const userData ={...profileData,...user};
+          console.log("userData : ",userData);
+          setUserProfile(userData);
+        } else {
+          router.push('/'); // Redirect to index if not logged in
         }
       } catch (error) {
         console.error("Error fetching profile:", error.message);
@@ -38,9 +57,9 @@ export default function Profile() {
         setLoading(false);
       }
     };
-
+  
     fetchProfile();
-  }, []); // Ajouter les dépendances nécessaires ici si besoin
+  }, []); // Dependencies array is empty as we want this to run only once on component mount
 
   return (
     <div className={styles.profileContainer}>
@@ -52,10 +71,11 @@ export default function Profile() {
 
       {loading ? (
         <p>Chargement du profil...</p>
-      ) : userProfile ? (
+      ) : isLoggedIn ? (
         <div>
-          <p>Email: {userProfile.email}</p>
-          {/* Ajoutez ici le reste du code pour afficher d'autres informations du profil */}
+          <p>Nom: {userProfile.nom}</p>
+          <p>Prénom: {userProfile.prénom}</p>
+          <p>Email: {userProfile.user.email}</p>
         </div>
       ) : (
         <p>Veuillez vous connecter pour accéder à votre profil.</p>
