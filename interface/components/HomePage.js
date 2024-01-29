bed6bc228e2cce13ba7f8b9981408804c4dc83e3
ElmoAlreadyTaken <71309060+misha7a@users.noreply.github.com>
2024-01-29 15:34:52 +0100

    feat: analyse dynamique du template présent dans l'IDE de l'interface web

--- interface/components/HomePage.js
+++ interface/components/HomePage.js
@@ -9,39 +9,52 @@ import 'ace-builds/src-noconflict/mode-c_cpp';
 import 'ace-builds/src-noconflict/theme-monokai';
 
 export default function HomePage(props) {
-  const [websocketData, setWebsocketData] = useState(null);
+  var template = `#include <espConfig.h>
+
+  void setup() {
+    Serial.begin(115200);
+    config.begin();
+  }
+
+  void loop() {
+    config.handleClient();
+    MDNS.update();
+  }`;
   const router = useRouter();
+  const [editorContent, setEditorContent] = useState(template)
+  const [boutonStyle, setBoutonStyle] = useState("text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700");
+  const [boutonTexte, setBoutonTexte] = useState("Téléverser");
 
   useEffect(() => {
-    // Remplacez 'wss://votre_adresse_ip_publique' par l'adresse de votre WebSocket
-    const ws = new WebSocket('wss://4.tcp.eu.ngrok.io:17707');
+    // Diviser le template en lignes ou en éléments clés
+    const lignesTemplate = template.split('\n');
+    // Vérifier si chaque ligne du template est présente dans le contenu de l'éditeur
+    const templateEstPresent = lignesTemplate.every(ligne => editorContent.includes(ligne.trim()));
+    if (templateEstPresent) {
+      setBoutonStyle("text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2");
+      setBoutonTexte("Téléverser");
+    } else {
+      setBoutonStyle("text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700");
+      setBoutonTexte("Doit contenir le template");
+    }
+  }, [editorContent]);
+  
+  const verifierContenu = () => {
+    // Diviser le template en lignes ou en éléments clés
+    const lignesTemplate = template.split('\n');
+    // Vérifier si chaque ligne du template est présente dans le contenu de l'éditeur
+    const templateEstPresent = lignesTemplate.every(ligne => editorContent.includes(ligne.trim()));
+    if (templateEstPresent) {
+      alert("La configuration ESP est présente dans le contenu.");
+      } else {
+      alert("Le contenu de l'éditeur doit au moins contenir le template de base.");
+      }
+  };
+  
 
-    ws.onopen = () => {
-      console.log('WebSocket Connected');
-    };
-
-    ws.onmessage = (event) => {
-      const message = JSON.parse(event.data);
-      setWebsocketData(message);
-    };
-
-    ws.onerror = (error) => {
-      console.error('WebSocket Error: ', error);
-    };
-
-    ws.onclose = () => {
-      console.log('WebSocket Disconnected');
-    };
-
-    // Nettoyer la connexion WebSocket lorsque le composant est démonté
-    return () => {
-      ws.close();
-    };
-  }, []);
-
-  function onChange(newValue) {
-    console.log('change', newValue);
-  }
+  const onChange = (newValue) => {
+    setEditorContent(newValue);
+  };
 
 
   return (
@@ -59,23 +72,22 @@ export default function HomePage(props) {
       <div className="fileUploadContainer">
         <FileUpload />
       </div>
-      {websocketData && (
-        <div>
-          {/* Afficher les données reçues via WebSocket */}
-          <label>Données WebSocket: {JSON.stringify(websocketData)}</label>
-        </div>
-      )}
       <div className="aceEditorContainer">
         <AceEditor
           mode="c_cpp"
           theme="monokai"
           onChange={onChange}
           name="UNIQUE_ID_OF_DIV"
+          value={editorContent}
           editorProps={{ $blockScrolling: true }}
           className="aceEditor"
         />
       </div>
-
+      <br></br>
+      
+      <button onClick={verifierContenu} class={boutonStyle}>
+        {boutonTexte}
+        </button>
     </div>
   );
 }
