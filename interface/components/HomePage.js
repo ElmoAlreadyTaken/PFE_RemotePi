7f5db70b6b963ca3603349d1132a074b9a7aaa5c
ElmoAlreadyTaken <71309060+misha7a@users.noreply.github.com>
2024-01-29 16:09:15 +0100

    feat : file from IDE upload through the RPI

--- interface/components/HomePage.js
+++ interface/components/HomePage.js
@@ -24,6 +24,8 @@ export default function HomePage(props) {
   const [editorContent, setEditorContent] = useState(template)
   const [boutonStyle, setBoutonStyle] = useState("text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700");
   const [boutonTexte, setBoutonTexte] = useState("Téléverser");
+  const [serverIP, setServerIP] = useState('4.tcp.eu.ngrok.io'); // Nouvelle adresse IP du serveur
+  const [portIP, setPortIP] = useState(17707); // Nouveau port
 
   useEffect(() => {
     // Diviser le template en lignes ou en éléments clés
@@ -39,16 +41,42 @@ export default function HomePage(props) {
     }
   }, [editorContent]);
   
-  const verifierContenu = () => {
+  const verifierContenu = async () => {
     // Diviser le template en lignes ou en éléments clés
     const lignesTemplate = template.split('\n');
     // Vérifier si chaque ligne du template est présente dans le contenu de l'éditeur
     const templateEstPresent = lignesTemplate.every(ligne => editorContent.includes(ligne.trim()));
     if (templateEstPresent) {
       alert("La configuration ESP est présente dans le contenu.");
+      try {
+        // Créer un Blob avec le contenu de l'éditeur
+        const blob = new Blob([editorContent], { type: 'text/plain' });
+    
+        // Créer un objet FormData et y ajouter le Blob en tant que fichier
+        const formData = new FormData();
+        formData.append('file', blob, 'monFichier.ino');
+    
+        // Utiliser l'API Fetch pour envoyer le fichier au serveur
+        const response = await fetch(`http://${serverIP}:${portIP}/upload`, {
+          method: "POST",
+          body: formData,
+        });
+    
+        if (response.ok) {
+          console.log("Fichier envoyé avec succès");
+          // Traitement supplémentaire en cas de succès
+        } else {
+          console.error("Échec de l'envoi du fichier");
+          // Gérer les erreurs ici
+        }
+      } catch (error) {
+        console.error("Erreur lors de l'envoi du fichier", error);
+        // Gérer les erreurs réseau ici
+      }
       } else {
       alert("Le contenu de l'éditeur doit au moins contenir le template de base.");
       }
+      
   };
   
 
