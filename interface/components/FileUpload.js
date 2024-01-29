2d9c891469036d4b74a0c9ff9b9f41bbcb3568ee
ElmoAlreadyTaken <71309060+misha7a@users.noreply.github.com>
2024-01-29 16:49:26 +0100

    feat : filter upload files to .ino from the input and the IDE send .ino files to the RPI

--- interface/components/FileUpload.js
+++ interface/components/FileUpload.js
@@ -9,28 +9,66 @@ export default function FileUpload() {
     const file = event.target.files[0];
     if (!file) return;
 
-    // Créer un objet FormData et y ajouter le fichier
-    const formData = new FormData();
-    formData.append("file", file);
+    // Vérifier l'extension du fichier
+  const fileExtension = file.name.split('.').pop();
+  if (fileExtension.toLowerCase() !== "ino") {
+    alert("Seuls les fichiers .ino sont acceptés.");
+    event.target.value = '';
+    return;
+  }
 
-    try {
-      // Utiliser l'API Fetch pour envoyer le fichier au serveur
-      const response = await fetch(`http://${serverIP}:${portIP}/upload`, {
-        method: "POST",
-        body: formData,
-      });
+      // Lire le contenu du fichier
+    const reader = new FileReader();
+    reader.readAsText(file);
 
-      if (response.ok) {
-        console.log("Fichier envoyé avec succès");
-        // Traitement supplémentaire en cas de succès
-      } else {
-        console.error("Échec de l'envoi du fichier");
-        // Gérer les erreurs ici
-      }
-    } catch (error) {
-      console.error("Erreur lors de l'envoi du fichier", error);
-      // Gérer les erreurs réseau ici
+    reader.onload = async (e) => {
+    const fileContent = e.target.result;
+
+    // Votre template
+    var template = `#include <espConfig.h>
+
+    void setup() {
+      Serial.begin(115200);
+      config.begin();
     }
+
+    void loop() {
+      config.handleClient();
+      MDNS.update();
+    }`;
+    const lignesTemplate = template.split('\n');
+    const templateEstPresent = lignesTemplate.every(ligne => fileContent.includes(ligne.trim()));
+
+    if (templateEstPresent) {
+      alert("La configuration ESP est présente dans le contenu.");
+      // Créer un objet FormData et y ajouter le fichier
+      const formData = new FormData();
+      formData.append("file", file);
+
+      try {
+        // Utiliser l'API Fetch pour envoyer le fichier au serveur
+        const response = await fetch(`http://${serverIP}:${portIP}/upload`, {
+          method: "POST",
+          body: formData,
+        });
+
+        if (response.ok) {
+          console.log("Fichier envoyé avec succès");
+          // Traitement supplémentaire en cas de succès
+        } else {
+          console.log("Échec de l'envoi du fichier");
+          // Gérer les erreurs ici
+        }
+      } catch (error) {
+        console.log("Erreur lors de l'envoi du fichier", error);
+        // Gérer les erreurs réseau ici
+      }
+    }else {
+      alert("Le contenu de l'éditeur doit au moins contenir le template de base.");
+    }
+  }; reader.onerror = (error) => {
+    console.log("Erreur lors de la lecture du fichier", error);
+  };
   }
 
   // Fonction pour gérer le changement de l'adresse IP du serveur
@@ -53,7 +91,7 @@ export default function FileUpload() {
         Server Port:
         <input type="number" value={portIP} onChange={handlePortIPChange} />
       </label>
-      <input type="file" onChange={handleUpload} />
+      <input type="file" accept=".ino"  onChange={handleUpload} />
     </div>
   );
 }
