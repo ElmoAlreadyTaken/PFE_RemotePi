101afedf2ff7110d4ac2bbb8fbca2210965522f2
Botkkai <quentin.sereno3@gmail.com>
2024-02-05 08:06:54 +0100

    fix:fixed npm run build error definitely

--- interface/components/Footer.js
+++ interface/components/Footer.js
@@ -1,27 +1,27 @@
 export default function Footer(){
-  return (
-    <footer class="custom-footer">
-        <div class="container ">
-            <div class="row">
-                <div class="col-lg-6 col-xs-12">
-                    <div class="left-text-content">
-                        <p>Copyright &copy; 2024 REMOTEPI Co., Ltd. 
-                        </p>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-xs-12">
-                    <div class="right-text-content">
-                            <ul class="social-icons">
-                                <li><p>Suivez-Nous</p></li>
-                                <li><a rel="nofollow" href=""><i class="fa fa-facebook"></i></a></li>
-                                <li><a rel="nofollow" href=""><i class="fa fa-twitter"></i></a></li>
-                                <li><a rel="nofollow" href=""><i class="fa fa-linkedin"></i></a></li>
-                                <li><a rel="nofollow" href=""><i class="fa fa-dribbble"></i></a></li>
-                            </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-  )
-}
+    return (
+      <footer class="custom-footer">
+          <div class="container ">
+              <div class="row">
+                  <div class="col-lg-6 col-xs-12">
+                      <div class="left-text-content">
+                          <p>Copyright &copy; 2024 REMOTEPI Co., Ltd. 
+                          </p>
+                      </div>
+                  </div>
+                  <div class="col-lg-6 col-xs-12">
+                      <div class="right-text-content">
+                              <ul class="social-icons">
+                              <li><p>Suivez-Nous</p></li>
+                              <li><a rel="nofollow" href=""><i class="fa fa-facebook"></i></a></li>
+                              <li><a rel="nofollow" href=""><i class="fa fa-twitter"></i></a></li>
+                              <li><a rel="nofollow" href=""><i class="fa fa-linkedin"></i></a></li>
+                              <li><a rel="nofollow" href=""><i class="fa fa-dribbble"></i></a></li>
+                              </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </footer>
+    )
+  }
