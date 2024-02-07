840adafae7e3a9909eff5619cee38016def032ec
Nassim.Si.Mohammed <nassim.si.mohammed@ibm.com>
2024-02-07 11:40:02 +0100

    fix : Téléversement

--- interface/components/HomePage.js
+++ interface/components/HomePage.js
@@ -13,12 +13,13 @@ export default function HomePage(props) {
   const [refreshKey, setRefreshKey] = useState(0);
   const [blink, setBlink] = useState(false);
   const router = useRouter();
-  const [baseURLServer, setbaseURLServer] = useState('');
-  const [serverPort, setServerPort] = useState('');
-  const [cameraPort, setCameraPort] = useState('');
-  const [baseURLCamera, setbaseURLCamera] = useState('');
+  const [baseURLServer, setbaseURLServer] = useState("");
+  const [serverPort, setServerPort] = useState("");
+  const [cameraPort, setCameraPort] = useState("");
+  const [baseURLCamera, setbaseURLCamera] = useState("");
   const [selectedRobotIdForLogs, setSelectedRobotIdForLogs] = useState(null);
-  
+  const [isUploading, setIsUploading] = useState(false);
+
   useEffect(() => {
     const fetchConfig = async () => {
       const { data, error } = await supabase
@@ -26,7 +27,7 @@ export default function HomePage(props) {
         .select("*")
         .single();
       if (data) {
-        console.log("[--] Supabase Data : ", data)
+        console.log("[--] Supabase Data : ", data);
         setbaseURLServer(data.baseURLServer);
         setbaseURLCamera(data.baseURLCamera);
         setServerPort(data.serverPort);
@@ -36,7 +37,7 @@ export default function HomePage(props) {
 
     fetchConfig();
   }, []);
-  
+
   const handleFileSent = () => {
     setIsFileSent(true);
     fetchLogs();
@@ -126,7 +127,8 @@ export default function HomePage(props) {
   }, [editorContent, selectedRobot]);
 
   const verifierContenu = async () => {
-    console.log()
+    console.log();
+    setIsUploading(true);
     const lignesTemplate = template.split("\n");
 
     const templateEstPresent = lignesTemplate.every((ligne) =>
@@ -141,8 +143,10 @@ export default function HomePage(props) {
         formData.append("file", blob, "monFichier.ino");
         formData.append("robotId", selectedRobot.id);
 
-        if (!baseURLServer || !serverPort){
-          console.log("[-] Erreur lors de l'envoi du fichier : pas de configuration de l'IP/PORT serveur")
+        if (!baseURLServer || !serverPort) {
+          console.log(
+            "[-] Erreur lors de l'envoi du fichier : pas de configuration de l'IP/PORT serveur"
+          );
           return;
         }
 
@@ -164,6 +168,7 @@ export default function HomePage(props) {
         "Le contenu de l'éditeur doit au moins contenir le template de base."
       );
     }
+    setIsUploading(false);
   };
 
   const onChange = (newValue) => {
@@ -302,7 +307,7 @@ export default function HomePage(props) {
           <div
             className="overflow-hidden bg-white shadow-xl sm:rounded-lg "
             style={{
-              height: "300px",
+              height: "330px",
               width: "550px",
               borderRadius: "20px",
               marginTop: "150px",
@@ -333,14 +338,18 @@ export default function HomePage(props) {
         </div>
         <button
           onClick={verifierContenu}
-          class={boutonStyle}
+          className={boutonStyle}
           style={{
             marginLeft: "675px",
             marginTop: "10px",
           }}
-          disabled={!selectedRobot}
+          disabled={!selectedRobot || isUploading}
         >
-          {boutonTexte}
+          {isUploading ? (
+            <div className="flex items-center">Téléversement...</div>
+          ) : (
+            boutonTexte
+          )}
         </button>
       </div>
     </>
